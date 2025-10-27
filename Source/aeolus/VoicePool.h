6f95dac80ed9f0ac2fba052d2e6c5837e6efbc1a
Wally Young <wallywyyoung@users.noreply.github.com>
2025-10-26 20:08:39 -0700

    Consolidation and cleanup

--- Source/aeolus/VoicePool.h
+++ Source/aeolus/VoicePool.h
@@ -1,8 +1,6 @@
 // ----------------------------------------------------------------------------
 //
 //  Copyright (C) 2025 Wally Young <wallywyyoung@users.noreply.github.com>
-//  Copyright (C) 2021 Arthur Benilov <arthur.benilov@gmail.com>
-//  Copyright (C) 2003-2013 Fons Adriaensen <fons@linuxaudio.org>
 //
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
