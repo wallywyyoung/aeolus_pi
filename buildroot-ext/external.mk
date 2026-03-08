918a657651749d60e6ce1dc18c5ffcb3129fd94d
Wally Young <wallywyyoung@users.noreply.github.com>
2026-03-08 16:36:31 -0400

    fixing buildroot-ext

--- buildroot-ext/external.mk
+++ buildroot-ext/external.mk
@@ -1,1 +1,11 @@
-include $(BR2_EXTERNAL_AEOLUS_PATH)/aeolus_pi.mk
+AEOLUS_PI_VERSION = 1.0
+AEOLUS_PI_SITE = $(BR2_EXTERNAL_AEOLUS_PATH)/..
+AEOLUS_PI_SITE_METHOD = local
+AEOLUS_PI_CONF_OPTS = \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_TOOLCHAIN_FILE=$(VCPKG_ROOT)/scripts/buildsystems/vcpkg.cmake \
+    -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$(HOST_DIR)/share/buildroot/toolchainfile.cmake \
+    -DVCPKG_TARGET_TRIPLET=arm64-linux \
+    -GNinja
+
+$(eval $(cmake-package))
