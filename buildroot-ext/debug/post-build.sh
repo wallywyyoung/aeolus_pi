f54853457ec7ecb37d65cb0e96ed69ff548bb077
Wally Young <wallywyyoung@users.noreply.github.com>
2026-03-15 11:13:10 -0700

    updating config build

--- buildroot-ext/debug/post-build.sh
+++ buildroot-ext/debug/post-build.sh
@@ -2,6 +2,6 @@
 set -e
 
 TARGET_DIR="$1"
-
+mkdir -p "$TARGET_DIR/boot"
 grep -q '/boot' "$TARGET_DIR/etc/fstab" || \
     echo -e "/dev/mmcblk0p1\t/boot\tvfat\tdefaults\t0\t0" >> "$TARGET_DIR/etc/fstab"
